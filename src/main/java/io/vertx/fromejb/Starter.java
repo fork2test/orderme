afd50e9d63e59aeb2a1b35b6d7efd2cd33cc237c
fiorenzo pizza <fiorenzino@gmail.com>
2015-11-11 13:43:34 +0100

    basic starter

--- src/main/java/io/vertx/fromejb/Starter.java
+++ src/main/java/io/vertx/fromejb/Starter.java
@@ -1,5 +1,9 @@
 package io.vertx.fromejb;
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
 /**
  * Created by fiorenzo on 11/11/15.
  */
